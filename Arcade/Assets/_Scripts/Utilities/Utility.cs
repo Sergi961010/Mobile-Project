12ab804e021b68014fdb4225af9b3d4bca93e9ed
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-05-15 12:47:04 +0200

    Added Utility class

---
+++ Arcade/Assets/_Scripts/Utilities/Utility.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TheCreators.Utilities
+{
+    public static class Utility
+    {
+	    public static T RandomEnumValue<T>()
+        {
+            var values = Enum.GetValues(typeof(T));
+            int random = UnityEngine.Random.Range(0, values.Length);
+            return (T)values.GetValue(random);
+        }
+    }
+}

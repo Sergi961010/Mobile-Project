9efc2cd32b0efa4454a60aa1ca5a7a83c1756c80
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-05-17 12:51:36 +0200

    Refactor

--- Arcade/Assets/_Scripts/Utilities/Utility.cs
+++ Arcade/Assets/_Scripts/Utilities/Utility.cs
@@ -4,11 +4,6 @@ namespace TheCreators.Utilities
 {
     public static class Utility
     {
-        public static int RandomValue(int min, int max)
-        {
-            int random = UnityEngine.Random.Range(min, max);
-            return random;
-        }
 	    public static T RandomEnumValue<T>()
         {
             var values = Enum.GetValues(typeof(T));

0d45bcc2b9299fec0dcbd881edc6b50843b75880
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-05-22 17:33:17 +0200

    Parallax logic

--- Arcade/Assets/_Scripts/Parallax.cs
+++ Arcade/Assets/_Scripts/Parallax.cs
@@ -1,31 +1,33 @@
+using TheCreators.Managers;
 using UnityEngine;
-using Cinemachine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera _vcam;
-    [SerializeField] private Transform _player;
-    private float _startPositionX;
-    private float _startZ, _spriteWidth;
-    private float DistanceFromPlayer => transform.position.z - _player.position.z;
-    private float ClippingPlane => _vcam.transform.position.z + (DistanceFromPlayer > 0 ? _vcam.m_Lens.FarClipPlane : _vcam.m_Lens.NearClipPlane);
-    private float ParallaxFactor => Mathf.Abs(DistanceFromPlayer / ClippingPlane);
-    private float TravelDistance => _vcam.transform.position.x * ParallaxFactor;
-    private float OffsetDistance => _vcam.transform.position.x * (1 - ParallaxFactor);
-    void Start()
-    {
-        _startPositionX = transform.position.x;
-        _startZ = transform.position.z;
-        _spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    private float _spriteWidth;
+    [SerializeField] private float _parallaxMultiplier;
 
-    }
-    void Update()
+    private void Start()
     {
-        transform.position = new Vector3(_startPositionX + TravelDistance, transform.position.y, _startZ);
-        if (OffsetDistance > _startPositionX + _spriteWidth) {
-            _startPositionX += _spriteWidth;
-        } else if (OffsetDistance < _startPositionX - _spriteWidth) {
-            _startPositionX -= _spriteWidth;
+        _spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        MoveElement();
+        CheckReset();
+    }
+
+    private void MoveElement()
+    {
+        float deltaX = GameManager.Instance.GameVelocity.x * Time.deltaTime * _parallaxMultiplier;
+        transform.position += new Vector3(-deltaX, 0, 0);
+    }
+
+    private void CheckReset()
+    {
+        if ((Mathf.Abs(transform.position.x) - _spriteWidth) > 0)
+        {
+            transform.position = new Vector3(0.0f, transform.position.y, transform.position.z);
         }
     }
 }

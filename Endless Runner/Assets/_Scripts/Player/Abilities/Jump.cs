cc374c85d4ff4c8d96c273b7e21f95728367a7be
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-09 11:55:57 +0200

    Moved jump logic into its own class
    Now the Jump class handles all the logic related with the player jump.
    
    I also added an scriptable object that holds all the data needed to calculate the jump according to a given max height of the jump and the time needed to reach the peak of the jump.

---
+++ Endless Runner/Assets/_Scripts/Player/Abilities/Jump.cs
@@ -0,0 +1,46 @@
+using TheCreators.CustomEventSystem;
+using TheCreators.ScriptableObjects;
+using UnityEngine;
+
+namespace TheCreators.Player.Abilities
+{
+    public class Jump : MonoBehaviour
+    {
+        [SerializeField] private JumpData _jumpData;
+        private CollisionSenses _collisionSenses;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _collisionSenses = GetComponentInChildren<CollisionSenses>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+        private void Start()
+        {
+            _rigidbody.gravityScale = _jumpData.gravityScale;
+        }
+
+        private void OnEnable()
+        {
+            GameEvent.OnPerformJump.AddListener(OnJumpInput);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_collisionSenses.Grounded)
+                _rigidbody.gravityScale = _jumpData.gravityScale;
+            else if (Mathf.Abs(_rigidbody.velocity.y) < _jumpData.jumpHangTimeThreshold)
+                _rigidbody.gravityScale = _jumpData.gravityScale * _jumpData.jumpHangGravityMultiplier;
+            else if (_rigidbody.velocity.y < 0)
+                _rigidbody.gravityScale = _jumpData.gravityScale * _jumpData.fallGravityMultiplier;
+        }
+
+        public void OnJumpInput()
+        {
+            if (_collisionSenses.Grounded)
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpData.jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}

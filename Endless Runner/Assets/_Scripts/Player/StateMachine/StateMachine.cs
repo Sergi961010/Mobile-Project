29889caf6514e58a1c0c60ade668d16684b0f984
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-14 11:47:18 +0200

    Update StateMachine.cs
    Definition of the fields and initialization of the first state.

--- Endless Runner/Assets/_Scripts/Player/StateMachine/StateMachine.cs
+++ Endless Runner/Assets/_Scripts/Player/StateMachine/StateMachine.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 
-namespace TheCreators
+namespace TheCreators.Player.StateMachine
 {
     public class StateMachine : MonoBehaviour
     {
-	
+        private State _currentState;
+        private StateFactory _stateFactory;
+
+        private void Awake()
+        {
+            _stateFactory = new StateFactory(this);
+            _currentState = _stateFactory.Run();
+            _currentState.EnterState();
+        }
     }
 }

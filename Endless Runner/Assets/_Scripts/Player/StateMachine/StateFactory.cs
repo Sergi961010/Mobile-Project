b9d0bd4592cdfe7a54b06767d354ce59c44a46c9
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-14 11:44:38 +0200

    Update StateFactory.cs
    Definition of the states that the factory will create.

--- Endless Runner/Assets/_Scripts/Player/StateMachine/StateFactory.cs
+++ Endless Runner/Assets/_Scripts/Player/StateMachine/StateFactory.cs
@@ -1,9 +1,29 @@
-using UnityEngine;
-
-namespace TheCreators
+namespace TheCreators.Player.StateMachine
 {
-    public class StateFactory : MonoBehaviour
+    public class StateFactory
     {
-	
+        StateMachine _context;
+
+        public StateFactory(StateMachine currentContext)
+        {
+            _context = currentContext;
+        }
+
+        public State Run()
+        {
+            return new RunState();
+        }
+        public State Jump()
+        {
+            return new JumpState();
+        }
+        public State Fly()
+        {
+            return new FlyState();
+        }
+        public State Dig()
+        {
+            return new DigState();
+        }
     }
 }

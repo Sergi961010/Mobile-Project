1f75e2b918616d431d12ca8c6c50ed174d7c8d4d
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-23 10:35:35 +0200

    Update Parallax.cs
    Now works with the main camera instead of the virtual camera.

--- Endless Runner/Assets/_Scripts/Enviroment/Parallax.cs
+++ Endless Runner/Assets/_Scripts/Enviroment/Parallax.cs
@@ -3,21 +3,21 @@ using Cinemachine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera _vcam;
+    private Camera _camera;
     [SerializeField] private Transform _player;
     private float _startPositionX;
     private float _startZ, _spriteWidth;
     private float DistanceFromPlayer => transform.position.z - _player.position.z;
-    private float ClippingPlane => _vcam.transform.position.z + (DistanceFromPlayer > 0 ? _vcam.m_Lens.FarClipPlane : _vcam.m_Lens.NearClipPlane);
+    private float ClippingPlane => _camera.transform.position.z + (DistanceFromPlayer > 0 ? _camera.farClipPlane : _camera.nearClipPlane);
     private float ParallaxFactor => Mathf.Abs(DistanceFromPlayer / ClippingPlane);
-    private float TravelDistance => _vcam.transform.position.x * ParallaxFactor;
-    private float OffsetDistance => _vcam.transform.position.x * (1 - ParallaxFactor);
+    private float TravelDistance => _camera.transform.position.x * ParallaxFactor;
+    private float OffsetDistance => _camera.transform.position.x * (1 - ParallaxFactor);
     void Start()
     {
         _startPositionX = transform.position.x;
         _startZ = transform.position.z;
         _spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-
+        _camera = Camera.main;
     }
     void Update()
     {
@@ -25,10 +25,6 @@ public class Parallax : MonoBehaviour
         if (OffsetDistance > _startPositionX + _spriteWidth)
         {
             _startPositionX += _spriteWidth;
-        }
-        else if (OffsetDistance < _startPositionX - _spriteWidth)
-        {
-            _startPositionX -= _spriteWidth;
         }
     }
 }

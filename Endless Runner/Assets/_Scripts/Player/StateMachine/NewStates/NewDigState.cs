947b99eff856e3adeee0c9f1e8ad83893ec5ff2d
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-28 11:49:35 +0200

    Create DigState.asset
    Dig State scriptable object asset.

---
+++ Endless Runner/Assets/_Scripts/Player/StateMachine/NewStates/NewDigState.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace TheCreators.Player
+{
+    [CreateAssetMenu(fileName = "DigState", menuName = "PlayerStates/Dig")]
+    public class NewDigState : NewPlayerState
+    {
+        public float _undergroundYPosition = -3.2f;
+        public float _surfaceYPosition = -2f;
+        public float duration = .5f;
+        public float elapsedTime;
+        public bool burrow;
+        public override void Enter()
+        {
+            Debug.Log("Enter Dig");
+            burrow = true;
+        }
+        private void HandleBurrow()
+        {
+            Vector2 newPosition = _context.transform.position;
+            float percentageComplete = elapsedTime / duration;
+            elapsedTime += Time.deltaTime;
+            newPosition.x = _context.transform.position.x + _context.RB.velocity.x * Time.deltaTime;
+            newPosition.y = Mathf.Lerp(_surfaceYPosition, _undergroundYPosition, percentageComplete);
+            _context.RB.isKinematic = true;
+            _context.RB.MovePosition(newPosition);
+
+            if (elapsedTime >= duration)
+            {
+                elapsedTime = 0;
+                burrow = false;
+            }
+        }
+        private void HandleUnburrow()
+        {
+            Vector2 newPosition = _context.transform.position;
+            float percentageComplete = elapsedTime / duration;
+            elapsedTime += Time.deltaTime;
+            newPosition.x = _context.transform.position.x + _context.RB.velocity.x * Time.deltaTime;
+            newPosition.y = Mathf.Lerp(_undergroundYPosition, _surfaceYPosition, percentageComplete);
+            _context.RB.MovePosition(newPosition);
+
+            if (elapsedTime >= duration)
+            {
+                _context.RB.isKinematic = false;
+                elapsedTime = 0;
+                _context.StateMachine.SwitchState(_context.runState);
+            }
+        }
+        public override void PhysicsUpdate()
+        {
+            if (burrow)
+                HandleBurrow();
+            if (_context.InputManager.SwipeDetection.UnburrowPerformed)
+            {
+                HandleUnburrow();
+            }
+        }
+    }
+}

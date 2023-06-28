0501445b3eba66473238520a73340899ee2770ae
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-06-28 11:50:17 +0200

    Refactor of all the states to use the CreateAsset attribute

---
+++ Endless Runner/Assets/_Scripts/Player/StateMachine/NewStates/NewFallState.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace TheCreators.Player
+{
+    [CreateAssetMenu(fileName = "FallState", menuName = "PlayerStates/Fall")]
+    public class NewFallState : NewPlayerState
+    {
+        public float fallGravityMultiplier = 3f;
+        public override void Enter()
+        {
+            ApplyGravityMultiplier();
+        }
+        public override void Exit()
+        {
+            CancelGravityMultiplier();
+        }
+        public override void LogicUpdate()
+        {
+            if (_context.CollisionSenses.Grounded)
+            {
+                _context.StateMachine.SwitchState(_context.runState);
+            }
+            if (_context.InputManager.FlyPerformed)
+            {
+                _context.StateMachine.SwitchState(_context.flyState);
+            }
+        }
+        private void ApplyGravityMultiplier()
+        {
+            _context.RB.gravityScale *= fallGravityMultiplier;
+        }
+        private void CancelGravityMultiplier()
+        {
+            _context.RB.gravityScale /= fallGravityMultiplier;
+        }
+    }
+}
---
+++ Endless Runner/Assets/_Scripts/Player/StateMachine/NewStates/NewRunState.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace TheCreators.Player
+{
+    [CreateAssetMenu(fileName = "RunState", menuName = "PlayerStates/Run")]
+    public class NewRunState : NewPlayerState
+    {
+        public float _speed = 6f;
+        public override void LogicUpdate()
+        {
+            if (_context.InputManager.JumpPerformed)
+            {
+                _context.StateMachine.SwitchState(_context.jumpState);
+                _context.InputManager.UseJumpInput();
+            }
+            if (_context.InputManager.SwipeDetection.BurrowPerformed)
+            {
+                _context.StateMachine.SwitchState(_context.digState);
+            }
+        }
+        public override void PhysicsUpdate()
+        {
+            _context.RB.velocity = new Vector2(_speed, 0);
+        }
+    }
+}

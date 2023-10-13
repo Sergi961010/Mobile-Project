fe329c577824351737918b5e20c9be0ac97bc30d
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-10-13 10:54:31 +0200

    Trigger death logic on player obstacle collision

--- Endless Runner/Assets/_Scripts/Core/CoreComponents/CollisionSenses.cs
+++ Endless Runner/Assets/_Scripts/Core/CoreComponents/CollisionSenses.cs
@@ -1,4 +1,4 @@
-using TheCreators.CustomEventSystem;
+using System.Collections;
 using UnityEngine;
 
 namespace TheCreators.CoreSystem.CoreComponents
@@ -19,7 +19,7 @@ namespace TheCreators.CoreSystem.CoreComponents
         private void OnTriggerEnter2D(Collider2D collider)
         {
             if (collider.gameObject.CompareTag("Obstacle"))
-                GameEvent.OnPlayerDeath.Invoke();
+                StartCoroutine(Core.Death.Die());
         }
     }
 }

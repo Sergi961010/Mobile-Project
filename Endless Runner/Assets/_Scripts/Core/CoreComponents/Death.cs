09fd12e1740c8fef67165174bb7d47bf7c20809d
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-10-24 12:49:31 +0200

    Death component disables input

--- Endless Runner/Assets/_Scripts/Core/CoreComponents/Death.cs
+++ Endless Runner/Assets/_Scripts/Core/CoreComponents/Death.cs
@@ -10,7 +10,7 @@ namespace TheCreators.CoreSystem.CoreComponents
         public UnityEvent DeathEvent;
         public IEnumerator Die()
         {
-            //Core.InputController.DisablePlayerControls();
+            Core.InputController.DisableInput();
             Core.PlayerAnimator.PlayAnimation(_animationClip);
             yield return new WaitForSeconds(_animationClip.length);
             Core.SpriteRenderer.SpriteRenderer.enabled = false;

adf650ca756b4060bf3c4c4b71d3efede4fe8426
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-11-02 12:54:30 +0100

    Add DigExit.cs

---
+++ Endless Runner/Assets/_Scripts/Core/CoreComponents/StateMachine/States/DigExit.cs
@@ -0,0 +1,40 @@
+using TheCreators.Utilities;
+using UnityEngine;
+
+namespace TheCreators.Player.StateMachine.States
+{
+    [CreateAssetMenu(fileName = "DigExitState", menuName = "PlayerStates/Dig/DigExit")]
+    public class DigExit : PlayerState
+    {
+        public float startYPosition = -3.4f;
+        public float endYPosition = -2;
+        private float _stateDuration;
+        public override void Enter()
+        {
+            _stateDuration = animations[0].length;
+            _context.PlayerAnimator.PlayAnimation(animations[0]);
+            _context.AudioController.PlayAudioEvent(audioEvent);
+            _context.Movement.StartHandleDigTranslationCoroutine(animations[0].length, startYPosition, endYPosition);
+        }
+        public override void Exit()
+        {
+            _context.Movement.Rigidbody.isKinematic = false;
+            _context.Stamina.CanRegenerate = true;
+            _context.SpriteRenderer.ChangeSortingOrder(0);
+        }
+        public override void LogicUpdate()
+        {
+            _stateDuration -= Time.deltaTime;
+            if (_stateDuration <= 0)
+                TransitionToRun();
+        }
+        public override void PhysicsUpdate()
+        {
+            
+        }
+        private void TransitionToRun()
+        {
+            _context.StateMachine.StateMachine.SwitchState(_context.StateMachine.runState);
+        }
+    }
+}

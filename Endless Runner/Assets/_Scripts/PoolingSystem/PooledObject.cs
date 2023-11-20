73ed4e513044f94c46de2b285ad2110613405763
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-11-20 09:50:05 +0100

    Update PooledObject.cs
    Added OnValidate to ensure the gameobject this script is attached to has the required components.

--- Endless Runner/Assets/_Scripts/PoolingSystem/PooledObject.cs
+++ Endless Runner/Assets/_Scripts/PoolingSystem/PooledObject.cs
@@ -1,21 +1,26 @@
-using TheCreators.Managers;
 using UnityEngine;
 
 namespace TheCreators.PoolingSystem
 {
+    [RequireComponent(typeof(Collider2D))]
     public class PooledObject : MonoBehaviour
     {
         private PoolsManager _poolsManager;
-
         private void Start()
         {
             _poolsManager = FindObjectOfType<PoolsManager>();
         }
-
         private void OnDisable()
         {
             if (_poolsManager != null)
-                _poolsManager.ReturnGameObject(gameObject);
+                _poolsManager.ReturnObjectToPool(gameObject);
+        }
+        private void OnValidate()
+        {
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            if (collider == null) collider = gameObject.AddComponent<BoxCollider2D>();
+
+            collider.isTrigger = true;
         }
     }
 }

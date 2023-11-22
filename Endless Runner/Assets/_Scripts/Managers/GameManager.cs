5565023ceace96bfad1ad56345772102ad694c48
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-11-22 11:04:59 +0100

    Update GameManager.cs

--- Endless Runner/Assets/_Scripts/Managers/GameManager.cs
+++ Endless Runner/Assets/_Scripts/Managers/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using TheCreators.PoolingSystem;
+using TheCreators.Spawners;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,15 +10,20 @@ namespace TheCreators.Managers
     {
         private bool _adDisplayed = false;
         [SerializeField] private UiManager _uiManager;
+        [SerializeField] private GameObject _player;
+        [SerializeField] private ObstacleSpawner _obstacleSpawner;
         public UnityEvent GameOverEvent;
 
-        public static float GameSpeed = 6f;
-        private void OnEnable()
+        public static float GameSpeed { get; private set; }
+        private Vector2 playerStartingPosition = new(-3.75f, -2f);
+        private void Start()
         {
             Application.targetFrameRate = 60;
+            GameSpeed = 6f;
         }
         public void OnPlayerDeath() => CheckIfShouldDisplayAd();
-        public void OnPlayerCollisionWithObstacle() { }
+        public void OnPlayerCollisionWithObstacle() => GameSpeed = 0f;
+        public void OnReward() => RestartGame();
         public void GameOver()
         {
             GameOverEvent.Invoke();
@@ -29,6 +37,18 @@ namespace TheCreators.Managers
             }
             else
                 GameOver();
+        }
+        private void RestartGame()
+        {
+            PooledObject[] obstaclesToClear = FindObjectsByType<PooledObject>(0);
+            foreach (var item in obstaclesToClear)
+            {
+                item.gameObject.SetActive(false);
+            }
+            GameSpeed = 6f;
+            _player.transform.position = playerStartingPosition;
+            _player.SetActive(true);
+            _obstacleSpawner.SpawnObstacle();
         }
     }
 }

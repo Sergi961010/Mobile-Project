f0619d4bfdbaaa2d3ae22ad11bda7cdcedbba143
Sergi961010 <sergi.borras.llaurado@gmail.com>
2023-11-28 10:34:37 +0100

    Update PauseMenu.cs
    Added events for pause and resume.

--- Endless Runner/Assets/_Scripts/UI/PauseMenu.cs
+++ Endless Runner/Assets/_Scripts/UI/PauseMenu.cs
@@ -1,5 +1,6 @@
 using TheCreators.Enums;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 namespace TheCreators.UI
@@ -7,16 +8,20 @@ namespace TheCreators.UI
     public class PauseMenu : MonoBehaviour
     {
         [SerializeField] private GameObject _pauseMenu;
+        [SerializeField] private UnityEvent _onPause;
+        [SerializeField] private UnityEvent _onResume;
         public void Pause()
         {
             _pauseMenu.SetActive(true);
             Time.timeScale = 0f;
+            _onPause.Invoke();
         }
 
         public void Resume()
         {
             _pauseMenu.SetActive(false);
             Time.timeScale = 1f;
+            _onResume.Invoke();
         }
 
         public void Home()
